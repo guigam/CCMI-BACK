eb621978bd9eb6d9e9f55bcd7f7672f226a9b898
guigam <m.guiga@gmail.com>
2023-10-25 09:18:26 -0400

    correction

--- inscription/admin.py
+++ inscription/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
 import inscription
+from inscription.models import Inscription, Parent, Enfant
 
 # Register your models here.
-# admin.site.register(inscription)
+admin.site.register(Inscription)
+admin.site.register(Parent)
+admin.site.register(Enfant)
